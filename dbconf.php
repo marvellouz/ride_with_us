48a68137652fb91efcf39f9317cf42e003d028df
Valentina Dinkova <vdinkova@fmi.uni-sofia.bg>
2011-05-21 23:13:12 +0300

    models

--- dbconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db['default']['dbdriver'] = "mysql";
-$db['default']['hostname'] = "localhost";
-$db['default']['username'] = "root";
-$db['default']['password'] = "";
-$db['default']['database'] = "ride_with_us";
-
-?>
