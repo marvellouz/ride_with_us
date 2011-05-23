9eb9780350ade3eb26e6c5ed55228be1d279f997
Lyubomir Popov <lhpopov@yahoo.com>
2011-05-23 20:32:32 +0300

    watch out for single quotation marks

--- templates_c/6f0a4a6e041b9f5e49e684213b6e5bb2492e1ab9.file.register.tpl.php
+++ templates_c/6f0a4a6e041b9f5e49e684213b6e5bb2492e1ab9.file.register.tpl.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.0.7, created on 2011-05-23 18:38:57
+<?php /* Smarty version Smarty-3.0.7, created on 2011-05-23 19:12:17
          compiled from "templates_c/register.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:132714dda7f91b0c0d7-33853884%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:131524dda87615f7905-04794061%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '6f0a4a6e041b9f5e49e684213b6e5bb2492e1ab9' => 
     array (
       0 => 'templates_c/register.tpl',
-      1 => 1306164958,
+      1 => 1306167082,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '132714dda7f91b0c0d7-33853884',
+  'nocache_hash' => '131524dda87615f7905-04794061',
   'function' => 
   array (
   ),
