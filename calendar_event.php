7cb103ce36ff9a72980dc6bcfdfbbb87df6e5763
Valentina Dinkova <vdinkova@fmi.uni-sofia.bg>
2011-05-24 16:00:09 +0300

    list of events for the given day

--- calendar_event.php
+++ calendar_event.php
@@ -12,7 +12,15 @@ function events($arr) {
   $month = $arr[1];
   $year = $arr[2];
   $date = "$year-$month-$day";
-  $day_events_query = "select * from ride_event where date(when_datetime) = '$date'";
+  $day_events_query = "select when_datetime, username as owner_username,
+		      fname as owner_fname,
+		      email as owner_email,
+		      start, end,
+		      re.id as event_id,
+		      re.additional_info as ride_info
+		      from ride_event re join user u on re.owner=u.id
+		      join route r on re.route=r.id 
+		      where date(when_datetime) = '$date'";
   $day_events = table_content($day_events_query);
   //print_r($day_events);
 
