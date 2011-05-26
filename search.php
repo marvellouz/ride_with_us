f549cff98857188861721c06f3af143ed735cae7
Lyubomir Popov <lhpopov@yahoo.com>
2011-05-26 04:07:21 +0300

    search by route details

--- search.php
+++ search.php
@@ -1,23 +1,101 @@
 ﻿<?php
 require_once("./helpers.php");
 
-//function search
-
-//$string="sad#///\\%^sj89";
-//echo $string . "   ------------<br />";
-
-function search_string($string)
+// key_words, takes only the key words that are entered, removing the nullable members of the $_POST array
+function key_words($words)
 {
-	global $mysqli;
+	$key_wds=array_filter($words);
+	array_pop($key_wds);
+	
+	return $key_wds;
+}
+//var_dump(key_words($_POST));
+
+function search_string()
+{
+	
+	global $webroot;
+	//global $mysqli;
 	//echo stripslashes($string);
 	//echo $mysqli->real_escape_string($string);
 	//echo sanitize_string($string);
+	if(isset($_POST['search_submit']))
+	{
+		$search_words=key_words($_POST);
+		
+		$select='';
+		$where='';
+		
+		$first=true;
+		//assigns the proper select columns and there matching values
+		//stackoverflow rulezzzz
+		foreach($search_words as $field => $value)
+		{	
+			if ($first) 
+			{
+				$first = false;
+				if($field=='displacement' || $field=='distance')
+				{
+					$where.= "$field=$value ";
+				}
+				else
+				{
+				$where .="$field like '%$value%' ";
+				// Do what you want to do before the first element
+				}
+			} 
+			else 
+			{
+				// Do what you want to do at the end of every element
+				// except the last, assuming the list has more than one element
+				$select .=',';
+				if($field=='displacement' || $field=='distance')
+				{
+					$where.= " and $field=$value ";
+				}
+				else
+				$where .=" and $field like '%$value%' ";
+			}
+			// Do what you want to do for the current element
+		$select .= $field;
+		}
+		
+		//var_dump($select);
+		//var_dump($where);
+		$query_search="SELECT displacement, distance, start, end, additional_info FROM route WHERE $where";
+		//$query_search="SELECT $select FROM route WHERE $where";
+		//var_dump($query_search);
+		$ride=table_content($query_search);
+		
+		if($ride)
+		{
+				
+			return array(
+			'assign' => array('ride' => $ride[0]),
+			'display' => 'templates_c/search.tpl');
+		//endif
+		}
+		else
+		{
+			$ride=NULL;
+			$ride_error="Няма намерени резултати";
+			return array(
+			'assign' => array('ride_error' => $ride_error, 'ride' => $ride),
+			'display' => 'templates_c/search.tpl');
+		}
+
+	//endif 
+	}
+	else 
+	{	//$ride = '';
+		$_SESSION['flash'] = "Не сте въвели дума/и за търсене!";
+		header("Location: {$webroot}/search");
+		return array(
+		'assign' => array(''),
+		'display' => 'templates_c/search.tpl');
+	}
 	
-	
-	
-	return array(
-	'assign' => array(''),
-	'display' => 'templates_c/search.tpl');
+
 }
 
 
--- templates_c/search.tpl
+++ templates_c/search.tpl
@@ -1,11 +1,51 @@
 ﻿{extends file="templates_c/index.tpl"}
 {block name=content}
 
-<form method="POST" action="search.php">
-<input type="text" name="search_string" id="search_box" />
-<input type="submit" name="search" value="search" />
+<form method="POST" action="search">
+<label for="search_string">Въведете критерий за търсене на маршрут:</label>
+<br /><br />
+<!-- <input type="text" name="search_string" id="search_box" /> -->
+
+<input type="text" name="start" />
+<label for="start">начална точка на карането</label>
+<br />
+<input type="text" name="end" />
+<label for="end">крайна точка на карането</label>
+<br />
+<input type="text" name="distance" />
+<label for="distance">дължина на карането</label>
+<br />
+<input type="text" name="displacement" />
+<label for="displacement">денивелация</label>
+<br />
+<input type="text" name="additional_info" />
+<label for="additional_info">описание</label>
+
+
+<br />
+<input type="submit" name="search_submit" value="Търси" />
 </form>
 <div id="found">
+{if isset($ride)}
+<table>
+	<tr>
+		<th>Денивелация</th>
+		<th>Дължина (км)</th>
+		<th>Начало</th>
+		<th>Край</th>
+		<th>Допълнителна информация</th>
+	</tr>
+	<tr>
+	{foreach from=$ride  item=rid}
+	
+		<td>{$rid}
+		</td>
+	{/foreach}
+	</tr>
+</table>
+{elseif isset($ride_error)}
+{$ride_error}
+{/if}
 
 </div>
 {/block}
